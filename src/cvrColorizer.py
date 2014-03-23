008a3996dc981632bd2b69f3c9cd09e2db876797
Gregory <grjordan@gmail.com>
2014-03-23 16:21:05 -0500

    Added the licence to the source files.

--- src/cvrColorizer.py
+++ src/cvrColorizer.py
@@ -2,6 +2,22 @@
 Created on Mar 6, 2014
 
 @author: Gregory Jordan
+
+Copyright (C) 2014  Gregory Jordan
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 '''
 import logging
 import CVR
